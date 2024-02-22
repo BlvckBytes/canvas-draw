e32c29213914eb54462a0e918691fbbb2b6151dd
Philip Hell <blvckbytes@gmail.com>
2024-02-22 16:12:00 +0100

    introduced SliderHandle.setStep

--- src/types/controls/slider-handle.interface.ts
+++ src/types/controls/slider-handle.interface.ts
@@ -2,5 +2,6 @@ export interface SliderHandle {
   setValue(value: number): SliderHandle;
   getValue(): number;
   setMinMax(min: number, max: number): SliderHandle;
+  setStep(step: number): SliderHandle;
   setText(text: string): SliderHandle;
 }
